11939716066283818152e87b9a54d57817d2b288
Chevannese <158519006+Chevannese@users.noreply.github.com>
2025-03-16 14:08:03 -0500

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,25 +1,59 @@
-package pack;
+// Jonathan Blackwood (2306822), 15/3/2025, Stack.java
+import java.util.*;
 
-public class Stack<T> extends LinkedList<T> {
-	public Stack() {
-		// TODO Auto-generated constructor stub
-	}
+public class Stack<T> implements Iterable<T> {
+    private Node<T> top;
+    
+    public Stack() {
+        this.top = null;
+    }
+    
+    public void add(T data) {
+        Node<T> top = new Node(data);
+        
+        if (this.top == null) {
+            this.top = top;
+        }
+        else {
+            top.next = this.top;
+            this.top = top;
+        }
+    }
+    
+    public T remove() {
+        if (this.top == null) {
+            return null;
+        }
+        else {
+            Node<T> top = this.top;
+            this.top = top.next;
+            return top.data;
+        }
+    }
+    
+    public StackItertor<T> iterator() {
+        return new StackItertor(this.top);
+    }
+}
 
-	public Stack(Node<T> head) {
-		super(head);
-		// TODO Auto-generated constructor stub
-	}
-
-	public Stack(T[] array) {
-		super(array);
-		// TODO Auto-generated constructor stub
-	}
-
-	public void add(T data) {
-		return super.insert(0, data);
-	}
-	
-	public T remove() {
-		return super.RemoveFirst();
-	}
+class StackItertor<T> implements Iterator<T> {
+    private Node<T> node;
+    
+    public StackItertor(Node<T> top) {
+        this.node = top;
+    }
+    
+    public T next() {
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        
+        Node<T> item = this.node;
+        this.node = item.next;
+        return item.data;
+    }
+    
+    public boolean hasNext() {
+        return this.node != null;
+    }
 }
