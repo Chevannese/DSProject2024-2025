62224e1670e784f74664f1d0df4278d4380f0030
Chevannese <158519006+Chevannese@users.noreply.github.com>
2025-03-16 13:13:51 -0500

    Update Queue.java
    Implements Iterable<T>

--- Queue.java
+++ Queue.java
@@ -1,42 +1,78 @@
-package pack;
+// Jonathan Blackwood (2306822), 15/3/2025, Queue.java
+import java.util.*;
 
-public class Queue<T> extends LinkedList<T> {
-	private Node<T> rear;
-	
-	public Queue() {
-		super();
-		this.rear = null;
-	}
-	
-	public void add(T data) {
-		if (this.head == null) {
-			this.head = new Node<T>(data);
-			this.rear = this.head;
-		}
-		else {
-			this.rear.next = new Node<T>(data);
-			this.rear = this.rear.next;
-		}
-	}
-	
-	public T remove(T data) {
-		if (this.head == null) {
-			return null;
-		}
-		else {
-			Node<T> node = this.head;
-			
-			if (node == this.rear) {
-				this.head = null;
-				this.rear = null;
-			}
-			else {
-				this.head = node.next;
-			}
-			return node.data;
-		}
-	}	
-	//Queues for patrons
-	//stacks for books
-	//
+public class Queue<T> implements Iterable<T> {
+    private Node<T> first;
+    private Node<T> last;
+    private int len;
+    
+    public Queue() {
+        this.first = null;
+        this.last = null;
+        this.len = 0;
+    }
+    
+    public int len() {
+        return this.len;
+    }
+    
+    public void add(T data) {
+        Node<T> node = new Node(data);
+        
+        if (this.last == null || this.first == null) {
+            this.last = node;
+            this.first = node;
+        }
+        else {
+            this.last.next = node;
+            this.last = node;
+        }
+        
+        this.len += 1;
+    }
+    
+    public T next() {
+        if (this.first == null) {
+            return null;
+        }
+        else {
+            Node<T> node = this.first;
+            if (this.first == this.last) {
+                this.first = null;
+                this.last = null;
+            }
+            else {
+                this.first = this.first.next;
+            }
+            
+            len -= 1;
+            return node.data;
+        }
+    }
+    
+    public QueueItertor iterator() {
+        return new QueueItertor(this.first);
+    }
+}
+
+class QueueItertor<T> implements Iterator<T> {
+    private Node<T> node;
+    
+    public QueueItertor(Node<T> first) {
+        this.node = first;
+    }
+    
+    public T next() {
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        
+        Node<T> item = this.node;
+        this.node = item.next;
+        return item.data;
+    }
+    
+    public boolean hasNext() {
+        return this.node != null;
+    }
 }
