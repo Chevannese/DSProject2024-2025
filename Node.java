91662225d4946f04c0caaa525f9081ebb801285c
Chevannese <158519006+Chevannese@users.noreply.github.com>
2025-03-16 13:11:26 -0500

    Update Node.java
    Node left and right were implemented

--- Node.java
+++ Node.java
@@ -1,13 +1,18 @@
-package pack;
+// Jonathan Blackwood (2306822), 20/2/2025, Node.java
 
+//package pack
 class Node<T> {
     public static final String ELEMENT_NODE = null;
 	public T data;
     public Node next;
+    public Node left;
+    public Node right;
     
     public Node(T data) {
         this.data = data;
         this.next = null;
+        this.left = null;
+        this.right = null;
     }
     
     public void display() {
